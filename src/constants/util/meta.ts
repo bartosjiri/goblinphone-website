3dd7ef4668105960b5b4ba0dd1b08c062073460b
bartosjiri <me@bartosjiri.com>
2022-06-12 15:09:04 +0200

    Add meta

--- src/constants/util/meta.ts
+++ src/constants/util/meta.ts
@@ -1,16 +1,16 @@
 import { PLATFORM_DOMAIN } from "./platform";
 
 export const META_TITLE = 'Goblin Phone';
-export const META_DESCRIPTION = '@TODO';
-export const META_KEYWORDS = ['goblin', 'goblintown', 'goblingrlz', 'phone'];
+export const META_DESCRIPTION = 'tʰₑ ⁿₑwₑsᵗ ᵖiᵉcᵉ ₒf tₑcʰ ᵸᵃs ₐʳʳᶤᵥₑd ᵢnto ᵗᵸₑ gᵒbˡᶤn tᵒwⁿ!';
+export const META_KEYWORDS = ['goblin', 'goblintown', 'goblingrlz', 'mcgoblin', 'phone'];
 
-export const META_THEME_COLOR = '@TODO';
-export const META_APPLE_COLOR = '@TODO';
-export const META_MICROSOFT_COLOR = '@TODO';
+export const META_THEME_COLOR = '#ffffff';
+export const META_APPLE_COLOR = '#aaa565';
+export const META_MICROSOFT_COLOR = '#aaa565';
 
 export const META_OG_URL = `https://${PLATFORM_DOMAIN}`;
 export const META_OG_IMAGE_URL = `${META_OG_URL}/meta/og-image.jpg`;
-export const META_OG_IMAGE_WIDTH = '@TODO';
-export const META_OG_IMAGE_HEIGHT = '@TODO';
+export const META_OG_IMAGE_WIDTH = 2215;
+export const META_OG_IMAGE_HEIGHT = 1160;
 
-export const META_VERSION = '@TODO';
+export const META_VERSION = 'd97kbruiw7';
