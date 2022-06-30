3e15d114bc978446a3decbf54af16c0e107fe5ef
bartosjiri <me@bartosjiri.com>
2022-07-01 00:12:22 +0200

    Update analytics

--- src/util/analytics/Analytics.svelte
+++ src/util/analytics/Analytics.svelte
@@ -2,15 +2,18 @@
 	import { onMount } from 'svelte';
 	import { browser } from '$app/env';
 	import { page } from '$app/stores';
+
 	import * as Fathom from 'fathom-client';
 
 	import { ANALYTICS_SCRIPT_URL } from '$constants/util/analytics';
 
-	onMount(async () => {
-		Fathom.load(import.meta.env.VITE_FATHOM_ID, {
-			url: ANALYTICS_SCRIPT_URL
+	const trackingId = import.meta.env.VITE_FATHOM_ID;
+	if (trackingId) {
+		onMount(async () => {
+			Fathom.load(trackingId, {
+				url: ANALYTICS_SCRIPT_URL
+			});
 		});
-	});
-
-	$: $page.url.pathname, browser && Fathom.trackPageview();
+	}
+	$: if (trackingId) $page.url.pathname, browser && Fathom.trackPageview();
 </script>
